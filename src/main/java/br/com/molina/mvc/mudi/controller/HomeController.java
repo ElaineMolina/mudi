25bfb16b4d800c23fb19063a025e979ebd88fab7
ElaineMolina <elainemolina10@gmail.com>
2022-08-15 18:44:05 -0300

    Integração com Spring Data

--- src/main/java/br/com/molina/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/molina/mvc/mudi/controller/HomeController.java
@@ -1,26 +1,22 @@
 package br.com.molina.mvc.mudi.controller;
 
 import br.com.molina.mvc.mudi.model.Pedido;
+import br.com.molina.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
-
+    @Autowired
+    private PedidoRepository pedidoRepository;
     @GetMapping("/home")
     public String home(Model model){
-        Pedido pedido =  new Pedido();
-        pedido.setNomeProduto("Xiaomi Redmi Note 8");
-        pedido.setUrlImagem("https://m.media-amazon.com/images/I/51kx1X-HduL._AC_SL1000_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07Z5BBG56");
-        pedido.setDescricao("uma descrição qualquer");
-
-        List<Pedido> pedidos = Arrays.asList(pedido);
-        model.addAttribute("pedidos", pedidos);
+            List<Pedido> pedidos = pedidoRepository.findAll();
+            model.addAttribute("pedidos", pedidos);
         return "home";
     }
 }
---
+++ src/main/java/br/com/molina/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,10 @@
+package br.com.molina.mvc.mudi.repository;
+
+import br.com.molina.mvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+}
